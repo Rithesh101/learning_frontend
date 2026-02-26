1b4cd8a3d46ba42a9aa54d1cf611e780300fb7d5
Rithesh Vanjari <rithesh1874@gmail.com>
2026-02-27 00:53:42 +0530

    basic import, dom

---
+++ react_latest/src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph.</p>
+  </div>
+);
